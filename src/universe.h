9144284d8bbc046c5806d8a202ef2c5ed8aff4be
Simon Howard <sdh300@ecs.soton.ac.uk>
2002-11-17 20:37:19 +0000

    initial doxygen documentation

--- src/universe.h
+++ src/universe.h
@@ -5,13 +5,34 @@
 #ifndef IRMO_UNIVERSE_H
 #define IRMO_UNIVERSE_H
 
+/*!
+ * \addtogroup universe
+ * \{
+ */
+
+//! An Irmo Universe.
+
 typedef struct _IrmoUniverse IrmoUniverse;
+
+/*!
+ * \brief A numerical object identifier
+ *
+ * All objects in a \ref IrmoUniverse have a unique number assigned to
+ * them. This can be used to refer to objects by their number. You can
+ * search for an object by number using the 
+ * \ref universe_get_object_for_id function.
+ */
+
 typedef guint irmo_objid_t;
 
 #include <glib.h>
 #include "callback.h"
 #include "if_spec.h"
 #include "object.h"
+
+//! \}
+
+// internals:
 
 // imposed by network protocol:
 
@@ -25,31 +46,89 @@ struct _IrmoUniverse {
 	int refcount;
 };
 
-// create a new universe from an interface spec
+/*!
+ * \addtogroup universe
+ * \{
+ */
+
+/*!
+ * \brief Create a new Universe
+ *
+ * Create a new Universe from an Interface Specification. The classes
+ * defined in the specification can then be instantiated as objects
+ * within the universe.
+ *
+ * \param spec	The Interface Specification to use.
+ * \return	The new universe.
+ */
 
 IrmoUniverse *universe_new(InterfaceSpec *spec);
 
-// find an object in the universe
+/*!
+ * \brief Find an object by its ID
+ *
+ * All objects within an IrmoUniverse have a unique number assigned to
+ * them (see \ref irmo_objid_t). This function searches for an object 
+ * by its identifier.
+ *
+ * \param universe	The Universe to search in
+ * \param id		The object number to search for
+ * \return		The IrmoObject or NULL if the object is not found.
+ */
 
 IrmoObject *universe_get_object_for_id(IrmoUniverse *universe,
 				       irmo_objid_t id);
 
-// iterate over objects in the universe
+/*!
+ * \brief Iterate over objects in a Universe
+ *
+ * This function allows you to iterate over objects in a Universe.
+ * For each object found, a function is called with a pointer to the
+ * object. The function can be used to iterate over ALL objects or
+ * just ones of a particular class.
+ *
+ * \param universe	The universe to iterate over
+ * \param classname	The name of the class of objects to iterate over.
+ * 			If you want to iterate over ALL objects, pass
+ * 			NULL for this value.
+ * \param func		The callback function to call.
+ * \param user_data	Extra data to pass to the functions when they 
+ * 			are called.
+ */
 
 void universe_foreach_object(IrmoUniverse *universe, gchar *classname,
 			     IrmoObjCallback func, gpointer user_data);
 
-// add reference
+/*!
+ * \brief	Add a reference to a Universe.
+ *
+ * Universes implement reference counting. Each time you store a 
+ * reference to a universe, call universe_ref to increase the reference
+ * count. When you remove a reference, call \ref universe_unref. The
+ * count starts at 1. When the count reaches 0, the universe is 
+ * destroyed.
+ *
+ * \param universe	The universe to reference
+ */
 
 void universe_ref(IrmoUniverse *universe);
 
-// remove reference
+/*!
+ * \brief	Remove a reference to a Universe.
+ *
+ * See \ref universe_ref.
+ *
+ * \param universe	The universe to unreference.
+ */
 
 void universe_unref(IrmoUniverse *universe);
 
 #endif /* #ifndef IRMO_UNIVERSE_H */
 
 // $Log: not supported by cvs2svn $
+// Revision 1.8  2002/11/13 14:14:46  sdh300
+// object iterator function
+//
 // Revision 1.7  2002/11/13 13:56:23  sdh300
 // add some documentation
 //
