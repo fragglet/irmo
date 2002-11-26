9221266e879df1549a9b12127120c05a5f7ae703
Simon Howard <sdh300@ecs.soton.ac.uk>
2002-11-26 16:30:44 +0000

    need ipv6 check around ipv6 sockaddr functions

--- src/netlib.h
+++ src/netlib.h
@@ -9,8 +9,10 @@
 guint sockaddr_in_hash(struct sockaddr_in *addr);
 gint sockaddr_in_cmp(struct sockaddr_in *a, struct sockaddr_in *b);
 
+#ifdef USE_IPV6
 guint sockaddr_in6_hash(struct sockaddr_in6 *addr);
 gint sockaddr_in6_cmp(struct sockaddr_in6 *a, struct sockaddr_in6 *b);
+#endif
 
 guint sockaddr_hash(struct sockaddr *addr);
 gint sockaddr_cmp(struct sockaddr *a, struct sockaddr *b);
@@ -18,6 +20,9 @@ gint sockaddr_cmp(struct sockaddr *a, struct sockaddr *b);
 #endif /* #ifndef IRMO_NETLIB_H */
 
 // $Log: not supported by cvs2svn $
+// Revision 1.2  2002/11/26 16:26:15  sdh300
+// missing glib.h include
+//
 // Revision 1.1  2002/11/26 16:23:28  sdh300
 // Split off sockaddr hash functions to a seperate netlib module
 //
