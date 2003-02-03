d5feb470a37b5d4e1da0fac24fa8a3e92ef4c3c6
Simon Howard <sdh300@ecs.soton.ac.uk>
2003-02-03 20:45:27 +0000

    add packet string code add packet writing code add packet flags

--- src/packet.c
+++ src/packet.c
@@ -4,6 +4,69 @@
 
 #include "packet.h"
 
+IrmoPacket *packet_new(gsize size)
+{
+	IrmoPacket *packet = g_new0(IrmoPacket, 1);
+
+	packet->data = malloc(size);
+	packet->size = size;
+	packet->pos = 0;
+
+	return packet;
+}
+
+void packet_free(IrmoPacket *packet)
+{
+	free(packet->data);
+	free(packet->src);
+	free(packet);
+}
+
+gboolean packet_writei8(IrmoPacket *packet, guchar c)
+{
+	if (packet->pos + 1 > packet->len)
+		return FALSE;
+
+	packet->data[packet->pos++] = c;
+
+	return TRUE;
+}
+
+gboolean packet_writei16(IrmoPacket *packet, guint16 s)
+{
+	if (packet->pos + 2 > packet->len)
+		return FALSE;
+
+	packet->data[packet->pos++] = (s >> 8) & 0xff;
+	packet->data[packet->pos++] = (s) & 0xff;
+
+	return TRUE;
+}
+
+gboolean packet_writei32(IrmoPacket *packet, guint32 l)
+{
+	if (packet->pos + 4 > packet->len)
+		return FALSE;
+
+	packet->data[packet->pos++] = (s >> 24) & 0xff;
+	packet->data[packet->pos++] = (s >> 16) & 0xff;
+	packet->data[packet->pos++] = (s >> 8) & 0xff;
+	packet->data[packet->pos++] = (s) & 0xff;
+
+	return TRUE;
+}
+
+gboolean packet_writestring(IrmoPacket *packet, gchar *s)
+{
+	if (packet->pos + strlen(s) + 1 > packet->len)
+		return FALSE;
+
+	strcpy(packet->data + packet->pos, s);
+	packet->pos += strlen(s);
+
+	return TRUE;
+}
+
 gboolean packet_readi8(IrmoPacket *packet, guchar *c)
 {
 	if (packet->pos + 1 > packet->len)
@@ -46,4 +109,21 @@ gboolean packet_readi32(IrmoPacket *packet, guint32 *l)
 	return TRUE;
 }
 
+gchar *packet_readstring(IrmoPacket *packet)
+{
+	guchar *start = packet->data + packet->pos;
+
+	while (packet->pos < packet->len) {
+		if (!packet->data[packet->pos])
+			return start;
+	}
+
+	// overflowed past the end of the packet
+	
+	return NULL;
+}
+
 // $Log: not supported by cvs2svn $
+// Revision 1.2  2002/12/02 21:34:49  sdh300
+// Add CVS tags
+//
--- src/packet.h
+++ src/packet.h
@@ -12,21 +12,42 @@ typedef struct _IrmoPacket IrmoPacket;
 
 #include "socket.h"
 
+#define	PACKET_FLAG_SYN 0x01
+#define	PACKET_FLAG_ACK 0x02
+#define	PACKET_FLAG_FIN 0x04
+#define	PACKET_FLAG_NEW 0x08
+#define	PACKET_FLAG_SET 0x10
+#define	PACKET_FLAG_DEL 0x20
+#define	PACKET_FLAG_FNC 0x40
+
 struct _IrmoPacket {
 	IrmoSocket *sock;       // socket this came from
 	struct sockaddr *src;   // source address
 	guchar *data;           // packet data
 	gsize len;              // data length
 	guint pos;              // current position in packet
+	IrmoClient *client;     // client implied by address (or NULL if none)
+	guint16 flags;          // flags from header
 };
+
+IrmoPacket *packet_new(gsize size);
+void packet_free(IrmoPacket packet);
+
+gboolean packet_writei8(IrmoPacket *packet, guchar c);
+gboolean packet_writei16(IrmoPacket *packet, guint16 s);
+gboolean packet_writei32(IrmoPacket *packet, guint32 l);
 
 gboolean packet_readi8(IrmoPacket *packet, guchar *c);
 gboolean packet_readi16(IrmoPacket *packet, guint16 *s);
 gboolean packet_readi32(IrmoPacket *packet, guint32 *l);
+gchar *packet_readstring(IrmoPacket *packet);
 
 #endif /* #ifndef IRMO_PACKET_H */
 
 // $Log: not supported by cvs2svn $
+// Revision 1.4  2002/12/02 22:24:22  sdh300
+// Initial socket run code/add extra data to packet objects
+//
 // Revision 1.3  2002/12/02 21:56:45  sdh300
 // Fix build (compile errors)
 //
