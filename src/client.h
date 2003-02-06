f8cecc7a2f8b90615fdce63adc9b53baa7490da9
Simon Howard <sdh300@ecs.soton.ac.uk>
2003-02-06 02:41:25 +0000

    Add CLIENT_DISCONNECTED for disconnected clients

--- src/client.h
+++ src/client.h
@@ -12,6 +12,7 @@ typedef struct _IrmoClient IrmoClient;
 typedef enum {
 	CLIENT_CONNECTING,             /* received first syn, sent syn ack */
 	CLIENT_CONNECTED,              /* received syn ack reply, normal op */
+	CLIENT_DISCONNECTED,           /* killed with syn fin */
 } IrmoClientState;
 
 struct _IrmoClient {
@@ -45,6 +46,9 @@ void client_disconnect(IrmoClient *client);
 #endif /* #ifndef IRMO_CLIENT_H */
 
 // $Log: not supported by cvs2svn $
+// Revision 1.2  2003/02/03 20:57:22  sdh300
+// Initial client code
+//
 // Revision 1.1  2002/11/26 15:23:49  sdh300
 // Initial network code
 //
