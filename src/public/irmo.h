638ec6c6659aec25b6ca8f98a92d23cd4733ab3b
Simon Howard <sdh300@ecs.soton.ac.uk>
2003-03-07 01:46:21 +0000

    Add a meta-include file to include all the others

---
+++ src/public/irmo.h
@@ -0,0 +1,18 @@
+
+#ifndef IRMO_H
+#define IRMO_H
+
+#include <irmo/callback.h>
+#include <irmo/client.h>
+#include <irmo/connection.h>
+#include <irmo/if_spec.h>
+#include <irmo/irmo.h>
+#include <irmo/object.h>
+#include <irmo/server.h>
+#include <irmo/socket.h>
+#include <irmo/universe.h>
+
+#endif /* #ifndef IRMO_H */
+
+// $Log: not supported by cvs2svn $
+
