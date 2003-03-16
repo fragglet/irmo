03214fb01406f32e824be2d360d69fad0d7702c9
Simon Howard <sdh300@ecs.soton.ac.uk>
2003-03-16 01:56:09 +0000

    Add method.h to irmo.h include

--- src/public/irmo.h
+++ src/public/irmo.h
@@ -6,6 +6,7 @@
 #include <irmo/client.h>
 #include <irmo/connection.h>
 #include <irmo/if_spec.h>
+#include <irmo/method.h>
 #include <irmo/object.h>
 #include <irmo/server.h>
 #include <irmo/socket.h>
@@ -14,6 +15,9 @@
 #endif /* #ifndef IRMO_H */
 
 // $Log: not supported by cvs2svn $
+// Revision 1.2  2003/03/07 12:45:27  sdh300
+// Dont include irmo/irmo.h
+//
 // Revision 1.1  2003/03/07 01:46:21  sdh300
 // Add a meta-include file to include all the others
 //
